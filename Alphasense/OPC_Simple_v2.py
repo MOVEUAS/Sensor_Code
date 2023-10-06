af987d36c22c497da0b74e58963e87f8c7cd4259
MOVEUAS <117048000+MOVEUAS@users.noreply.github.com>
2023-10-06 15:58:55 -0400

    Update OPC_Simple_v2.py
    Added the fully formatted dataStr, needs to be tested!

--- Alphasense/OPC_Simple_v2.py
+++ Alphasense/OPC_Simple_v2.py
@@ -41,12 +41,59 @@ while True:
         continue
 
 
-    dataStr1 = ', {0}, {1}, {2},    {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}'.format(recorded_data['Bin0'],
+    # dataStr1 = ', {0}, {1}, {2},    {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}'.format(recorded_data['Bin0'],
+    # dateStr =', Date:, {0}'.format(datetime.now())
+
+
+        dataStr = ', {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, {23}, {24}, {25}, {26}, {27}, {28}, {29}, {30}, {31}, {32}, {33}, {34}, {35}, {36}, {37}, {38}, {39}, {40}, {41}, {42}, {43}, {44}, {45}, {46}, {47}, {48}, {49}, {50}, {51}, {52}, {53}'.format(
+                recorded_data.get('Bin 0', ''),
+                recorded_data.get('Bin 1', ''),
+                recorded_data.get('Bin 2', ''),
+                recorded_data.get('Bin 3', ''),
+                recorded_data.get('Bin 4', ''),
+                recorded_data.get('Bin 5', ''),
+                recorded_data.get('Bin 6', ''),
+                recorded_data.get('Bin 7', ''),
+                recorded_data.get('Bin 8', ''),
+                recorded_data.get('Bin 9', ''),
+                recorded_data.get('Bin 10', ''),
+                recorded_data.get('Bin 11', ''),
+                recorded_data.get('Bin 12', ''),
+                recorded_data.get('Bin 13', ''),
+                recorded_data.get('Bin 14', ''),
+                recorded_data.get('Bin 15', ''),
+                recorded_data.get('Bin 16', ''),
+                recorded_data.get('Bin 17', ''),
+                recorded_data.get('Bin 18', ''),
+                recorded_data.get('Bin 19', ''),
+                recorded_data.get('Bin 20', ''),
+                recorded_data.get('Bin 21', ''),
+                recorded_data.get('Bin 22', ''),
+                recorded_data.get('Bin 23', ''),
+                recorded_data.get('Bin1 MToF', ''),
+                recorded_data.get('Bin3 MToF', ''),
+                recorded_data.get('Bin5 MToF', ''),
+                recorded_data.get('Bin7 MToF', ''),
+                recorded_data.get('Sampling Period', ''),
+                recorded_data.get('SFR', ''),
+                recorded_data.get('Temperature', ''),
+                recorded_data.get('Relative humidity', ''),
+                recorded_data.get('PM1', ''),
+                recorded_data.get('PM2.5', ''),
+                recorded_data.get('PM10', ''),
+                recorded_data.get('#RejectGlitch', ''),
+                recorded_data.get('#RejectLongTOF', ''),
+                recorded_data.get('#RejectRatio', ''),
+                recorded_data.get('#RejectOutOfRange', ''),
+                recorded_data.get('Fan rev count', ''),
+                recorded_data.get('Laser status', ''),
+                recorded_data.get('Checksum', '')
+        )
+
     dateStr =', Date:, {0}'.format(datetime.now())
+
     file.write(dateStr + dataStr + "\n")
     file.flush()
-
-
 
 
 #This is what the bins apparently are:
