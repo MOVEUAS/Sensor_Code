e0823fabd06e0a01e15d31428113bc2880797690
MOVEUAS <117048000+MOVEUAS@users.noreply.github.com>
2023-12-13 12:50:33 -0500

    Update OPC_Simple_v2.py
    comfirmed order & documented data order

--- Alphasense/OPC_Simple_v2.py
+++ Alphasense/OPC_Simple_v2.py
@@ -1,5 +1,6 @@
-#Created by: Kaleb Nails, Erik Liebergall
-#Date: 10/6/2023
+#Created by: Kaleb Nails, Erik Liebergall, Marc Compere
+#created : 10/6/2023
+#modified: 13 Dec 2023
 #from time import sleep
 from usbiss.spi import SPI
 import opcng as opc
@@ -104,7 +105,59 @@ while DataLoop ==True:
         recorded_data = dev.histogram()
         print(recorded_data)
 
-        dataStr = ', {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, {23}, {24}, {25}, {26}, {27}, {28}, {29}, {30}, {31}, {32}, {33}, {34}, {35}, {36}, {37}, {38}, {39}, {40}, {41}'.format(recorded_data.get('Bin 0', ''),recorded_data.get('Bin 1', ''),recorded_data.get('Bin 2', ''),recorded_data.get('Bin 3', ''),recorded_data.get('Bin 4', ''),recorded_data.get('Bin 5', ''),recorded_data.get('Bin 6', ''),recorded_data.get('Bin 7', ''),recorded_data.get('Bin 8', ''),recorded_data.get('Bin 9', ''),recorded_data.get('Bin 10', ''),recorded_data.get('Bin 11', ''),recorded_data.get('Bin 12', ''),recorded_data.get('Bin 13', ''),recorded_data.get('Bin 14', ''),recorded_data.get('Bin 15', ''),recorded_data.get('Bin 16', ''),recorded_data.get('Bin 17', ''),recorded_data.get('Bin 18', ''),recorded_data.get('Bin 19', ''),recorded_data.get('Bin 20', ''),recorded_data.get('Bin 21', ''),recorded_data.get('Bin 22', ''),recorded_data.get('Bin 23', ''),recorded_data.get('Bin1 MToF', ''),recorded_data.get('Bin3 MToF', ''),recorded_data.get('Bin5 MToF', ''),recorded_data.get('Bin7 MToF', ''),recorded_data.get('Sampling Period', ''),recorded_data.get('SFR', ''),recorded_data.get('Temperature', ''),recorded_data.get('Relative humidity', ''),recorded_data.get('PM1', ''),recorded_data.get('PM2.5', ''),recorded_data.get('PM10', ''),recorded_data.get('#RejectGlitch', ''),recorded_data.get('#RejectLongTOF', ''),recorded_data.get('#RejectRatio', ''),recorded_data.get('#RejectOutOfRange', ''),recorded_data.get('Fan rev count', ''),recorded_data.get('Laser status', ''),recorded_data.get('Checksum', ''))
+        dataStr = ', {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, {23},\
+		             {24}, {25}, {26}, {27},\
+					 {28}, {29}, {30}, {31},\
+					 {32}, {33}, {34}, \
+					 {35}, {36}, {37}, {38}, {39}, {40}, {41}'.format(
+		recorded_data.get('Bin 0', ''),
+		recorded_data.get('Bin 1', ''),
+		recorded_data.get('Bin 2', ''),
+		recorded_data.get('Bin 3', ''),
+		recorded_data.get('Bin 4', ''),
+		recorded_data.get('Bin 5', ''),
+		recorded_data.get('Bin 6', ''),
+		recorded_data.get('Bin 7', ''),
+		recorded_data.get('Bin 8', ''),
+		recorded_data.get('Bin 9', ''),
+		recorded_data.get('Bin 10', ''),
+		recorded_data.get('Bin 11', ''),
+		recorded_data.get('Bin 12', ''),
+		recorded_data.get('Bin 13', ''),
+		recorded_data.get('Bin 14', ''),
+		recorded_data.get('Bin 15', ''),
+		recorded_data.get('Bin 16', ''),
+		recorded_data.get('Bin 17', ''),
+		recorded_data.get('Bin 18', ''),
+		recorded_data.get('Bin 19', ''),
+		recorded_data.get('Bin 20', ''),
+		recorded_data.get('Bin 21', ''),
+		recorded_data.get('Bin 22', ''),
+		recorded_data.get('Bin 23', ''),
+		recorded_data.get('Bin1 MToF', ''),         # 24
+		#recorded_data.get('Bin2 MToF', ''),
+		recorded_data.get('Bin3 MToF', ''),         # 25
+		#recorded_data.get('Bin4 MToF', ''),
+		recorded_data.get('Bin5 MToF', ''),         # 26
+		#recorded_data.get('Bin6 MToF', ''),
+		recorded_data.get('Bin7 MToF', ''),         # 27
+		
+		recorded_data.get('Sampling Period', ''),   # 28
+		recorded_data.get('SFR', ''),               # 29
+		recorded_data.get('Temperature', ''),       # 30
+		recorded_data.get('Relative humidity', ''), # 31
+		
+		recorded_data.get('PM1', ''),               # 32 (ug/m3), datasheet pdf page 13
+		recorded_data.get('PM2.5', ''),             # 33 (ug/m3)
+		recorded_data.get('PM10', ''),              # 34 (ug/m3)
+		
+		recorded_data.get('#RejectGlitch', ''),     # 35
+		recorded_data.get('#RejectLongTOF', ''),    # 36
+		recorded_data.get('#RejectRatio', ''),      # 37
+		recorded_data.get('#RejectOutOfRange', ''), # 38
+		recorded_data.get('Fan rev count', ''),     # 39
+		recorded_data.get('Laser status', ''),      # 40
+		recorded_data.get('Checksum', ''))          # 41
 
         dateStr =', Date:, {0}'.format(datetime.now())
 
@@ -117,3 +170,4 @@ while DataLoop ==True:
     except RuntimeError:
         print("Unable to read from sensor, retrying...")
         continue
+
